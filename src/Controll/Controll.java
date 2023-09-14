63c68662f5a0e63fc8b92d6db0e2b0fc8e2c7fb3
FPTShop <FPTShop@LAPTOP-E6O8DUU0>
2023-09-14 10:47:38 +0700

    First commit 1

---
+++ src/Controll/Controll.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Controll;
+
+/**
+ *
+ * @author FPTShop
+ */
+import java.util.Scanner;
+import java.util.Map;
+
+public class Controll {
+   public static void main(String[] args) {
+       String userInput;
+       try (Scanner scanner = new Scanner(System.in)) {
+           System.out.print("Enter your comment: ");
+           userInput = scanner.nextLine();
+       }
+
+        Count analyzer = new Count(userInput);
+
+        Map<String, Integer> wordCountMap = analyzer.countWords();
+// Gọi phương thức countWords để đếm từ và lưu kết quả trong map.
+        System.out.println(formatMap(wordCountMap));
+
+        Map<Character, Integer> letterCountMap = analyzer.countLetters();//Gọi phương thức countLetters để đếm chữ cái và lưu kết quả trong map.
+        System.out.println(formatMap(letterCountMap));
+    }
+
+    private static <K, V> String formatMap(Map<K, V> map) {
+        StringBuilder builder = new StringBuilder("{ ");//Tạo một đối tượng StringBuilder để xây dựng chuỗi kết quả.
+       // Duyệt qua map và thêm chúng vào chuỗi kết quả.
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            builder.append(entry.getKey()).append(" = ").append(entry.getValue()).append(", ");
+        }
+        builder.setLength(builder.length() - 2);  // Loại bỏ ký tự ", " cuối cùng.
+        builder.append(" }");// Đóng chuỗi kết quả với " }"
+        return builder.toString();// Trả về chuỗi kết quả đã định dạng.
+    }
+}
